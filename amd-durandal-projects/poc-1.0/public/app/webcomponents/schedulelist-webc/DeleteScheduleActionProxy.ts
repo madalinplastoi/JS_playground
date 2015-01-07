d7db4425ea99c425d77b9b7b394acef7e1df9bd7
Tut Andrei <andrei.tut@synergogroup.net>
2015-01-07 15:39:29 +0200

    changes.

---
+++ amd-durandal-projects/poc-1.0/public/app/webcomponents/schedulelist-webc/DeleteScheduleActionProxy.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by tzutz on 07.01.2015.
+ */
+import actionProxyRef = require('../../utils/ActionProxy');
+export class DeleteScheduleActionProxy extends actionProxyRef.ActionProxy{
+
+}
