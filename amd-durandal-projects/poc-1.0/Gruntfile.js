9f9db0709b2dd18389777b8d221bdcb3e127b190
Tut Andrei <andrei.tut@synergogroup.net>
2015-01-09 09:04:11 +0200

    worked on grunt durandal.

--- amd-durandal-projects/poc-1.0/Gruntfile.js
+++ amd-durandal-projects/poc-1.0/Gruntfile.js
@@ -35,6 +35,35 @@ module.exports = function (grunt) {
                     optimize: 'uglify'
                 }
             }
+        },
+
+        durandal: {
+            dist: {
+                src: [
+                    "public/app/**/*.js",
+                    "public/app/*.js"
+
+                ],
+                options: {
+                    name: '../lib/require/almond-custom',
+                    baseUrl: "public/app/",
+                    mainPath: "public/app/main.js",
+                    out: "public/build/js/main-built.js",
+                    paths: {
+                        text: '../lib/require/text',
+                        durandal: '../lib/durandal/js',
+                        plugins: '../lib/durandal/js/plugins',
+                        transitions: '../lib/durandal/js/transitions',
+                        jquery: '../lib/jquery/jquery-1.9.1',
+                        knockout: '../lib/knockout/knockout-3.1.0',
+                        kocommand : '../lib/kolite/knockout.command'
+
+                    },
+                    optimize: 'none',
+
+
+                }
+            }
         }
     });
 
@@ -42,8 +71,9 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-durandal');
 
     grunt.registerTask('default', ['clean']);
     grunt.registerTask('stage', ['clean', 'cssmin']);
-    grunt.registerTask('prod', ['clean', 'cssmin', 'requirejs']);
+    grunt.registerTask('prod', ['clean', 'cssmin', 'durandal']);
 }
